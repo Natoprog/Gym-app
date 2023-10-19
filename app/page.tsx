b5fc1d285b3b88c96b33d2dae0e3314570e51567
Natoprog <natan.lucz@gmail.com>
2023-10-20 00:10:51 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import { getServerSession } from 'next-auth'
 import { getXataClient } from '../utils/xata'
+import Nextauth from '@/pages/api/auth/[...nextauth]'
+import LoginBtn from '@/components/LoginBtn'
 
 
 export default async function Home() {
-  
+
+  const session = await getServerSession(Nextauth)
+
   return (
-    <h1>Hello</h1>
+    <main className='flex justify-center items-center w-screen h-screen gap-10'>
+      {session ? (<h1>Logged in as </h1>) : (<LoginBtn/>)}
+      <h1>Hello</h1>
+    </main>
   )
 }
---
+++ components/LoginBtn.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { signIn } from 'next-auth/react'
+
+
+export default async function LoginBtn() {
+
+  return (
+    <form onSubmit={(e) => {
+        e.preventDefault()
+        signIn('google', { callbackUrl: 'http://localhost:3000/success'})
+    }}>
+        <button type="submit" className='p-2 rounded-md bg-teal-600 text-white'>Login</button>
+    </form>
+  )
+}

3b736ed63ccf9468d44cfa6221504a2ad489cc20
Natoprog <natan.lucz@gmail.com>
2023-12-08 18:38:43 +0100

    fix formating of auth

--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
 export const {
-  handlers: {GET, POST},
+  handlers: { GET, POST },
   auth,
   signIn,
   signOut,
--- src/context/client-provider.tsx
+++ src/context/client-provider.tsx
@@ -1,10 +1,6 @@
-"use client"
+"use client";
 import { SessionProvider } from "next-auth/react";
 
-export default function Provider({
-  children
-}: {
-  children: React.ReactNode;
-}){
+export default function Provider({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }

3fe20d4473af00400ee6309a17fab3f9e65867ca
Natoprog <natan.lucz@gmail.com>
2023-12-08 18:37:25 +0100

    Add session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navigation from '../components/UI/Navigation/Navigation'
+import Provider from '../context/client-provider'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,8 +15,10 @@ export default function RootLayout({
   return (
     <html lang="pl">
         <body className={inter.className}>
+          <Provider>
             {children}
             <Navigation />
+            </Provider>
         </body>
     </html>
   )

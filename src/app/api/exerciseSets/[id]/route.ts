d1b81ac898b48b9b7d5c9723cc9513c40f66293c
Natoprog <natan.lucz@gmail.com>
2025-03-03 22:52:46 +0100

    Add awiat before params

--- src/app/api/exerciseSets/[id]/route.ts
+++ src/app/api/exerciseSets/[id]/route.ts
@@ -8,7 +8,7 @@ export async function GET(
   { params }: { params: { id: string } } // Correctly typed params
 ) {
   try {
-    const { id } = params; // Destructure id from params directly
+    const { id } = await params; // Destructure id from params directly
     const exerciseId = parseInt(id);
 
     if (isNaN(exerciseId)) {
